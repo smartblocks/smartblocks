a72d36daa2cf0bac2e6c77a1185759e0b56996f9
Antoine Jackson <a.j.william26@gmail.com>
2014-01-10 18:30:26 +0100

    ~ small corrections for less

--- smartblocks/controllers/Blocks.js
+++ smartblocks/controllers/Blocks.js
@@ -3,14 +3,17 @@ var SmartBlocks = require('../smartblocks');
 module.exports = {
     index: function (req, res) {
         var blocks = SmartBlocks.getBlocksModels();
-//        var response = [];
-//        for (var k in blocks) {
-//            var block = {
-//                name: blocks[k]
-//            };
-//            response.push(block);
-//        }
-
         res.json(blocks);
+    },
+    style_list: function (req, res) {
+        var blocks = SmartBlocks.getBlocks();
+        console.log("JAHA");
+        var response = '@import "/main.less";';
+
+        for (var k in blocks) {
+            response += '\n@import "/' + blocks[k] + '/index.less";';
+        }
+        res.setHeader('content-type', 'text/css');
+        res.send(200, response);
     }
 };
