fd080905fdfa3cfa4e065c52ad564edd9b0a0207
Antoine Jackson <a.j.william26@gmail.com>
2014-01-18 01:17:51 +0100

    + Added node-orm base supprt. Changed cli file

--- app.js
+++ app.js
@@ -8,173 +8,182 @@ module.exports = function () {
     var path = require('path');
     var fs = require('fs');
     var os = require('os');
+    var orm = require('orm');
 
     var app = express();
 
     var config = require(path.join(process.cwd(), 'config'))();
 
-// all environments
+
+
     app.set('port', config.port || process.env.PORT || 3000);
-//    app.set('port', process.env.PORT || 3000);
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'hjs');
+    console.log(config.database.connection_str);
+    app.use(orm.express(config.database.connection_str, {
+        define: function (db, models, next) {
+            console.log('registering users');
+            models.User = db.define('User', {
+                email: String,
+                password: String,
+                session_id: String
+            }, {
+                methods: {
+
+                }
+            });
+            next();
+        }
+    }));
+
+
     app.use(express.favicon());
-//    app.use(express.logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded());
+    app.use(require('connect-multiparty')());
     app.use(express.methodOverride());
     app.use(express.cookieParser('your secret here'));
     app.use(express.session());
-//    app.use(express.bodyParser());
+
+
     app.use(app.router);
     app.use(require('less-middleware')({ src: path.join(__dirname, 'public/build') }));
     app.use(express.static(path.join(__dirname, 'public/build')));
 
-// development only
+
     if ('development' == app.get('env')) {
         app.use(express.errorHandler());
     }
 
-// [begin] Connection to DataBase depending on dbdriver defined in config/index.js   ****/
-    var db_driver = require(path.join(__dirname, 'lib', 'data_access', "dbdriver_" + config.database.dbdriver));
-    db = db_driver.connect(config);
-    db.on('error', console.error.bind(console, 'Database connection problem: '));
-    db.once('open', function () {
-        var attachDB = function (req, res, next) {
-            req.db = db;
-            next();
-        };
-//  [end] Connection to DataBase depending on dbdriver defined in config/index.js   ****/
 
 // [begin] Implementing RESTful Web Services with controllers in lib and backend folder.  ****/
-        var blocks_folders = fs.readdirSync(path.join(process.cwd(), 'blocks'));
+    var blocks_folders = fs.readdirSync(path.join(process.cwd(), 'blocks'));
 //backend blocks webservices
-        for (var k in blocks_folders) {
-            var block_folder = blocks_folders[k];
-            var frontend_path = path.join(process.cwd(), 'blocks', block_folder, 'frontend');
-            var backend_path = path.join(process.cwd(), 'blocks', block_folder, 'backend');
-            app.use('/' + block_folder, express.static(frontend_path));
-            var controllers = fs.readdirSync(path.join(backend_path, 'controllers'));
+    for (var k in blocks_folders) {
+        var block_folder = blocks_folders[k];
+        var frontend_path = path.join(process.cwd(), 'blocks', block_folder, 'frontend');
+        var backend_path = path.join(process.cwd(), 'blocks', block_folder, 'backend');
+        app.use('/' + block_folder, express.static(frontend_path));
+        var controllers = fs.readdirSync(path.join(backend_path, 'controllers'));
 
-            for (var c in controllers) {
-                var controller_name = controllers[c].replace('.js', '');
-                var controller = require(path.join(process.cwd(), 'blocks', block_folder, 'backend', 'controllers', controller_name));
-                (function (controller_name, controller) {
-                    app.get('/' + block_folder + '/' + controller_name, attachDB, function (req, res) {
-                        if (controller.index) {
-                            controller.index(req, res);
-                        }
-                    });
-                    app.all('/' + block_folder + '/' + controller_name + '/action/:action', attachDB, function (req, res) {
-                        if (controller[req.params.action]) {
-                            controller[req.params.action](req, res);
-                        }
-                    });
-                    app.get('/' + block_folder + '/' + controller_name + '/:id', attachDB, function (req, res) {
-                        if (controller.show) {
-                            controller.show(req, res);
-                        }
-                    });
-                    app.post('/' + block_folder + '/' + controller_name, attachDB, function (req, res) {
-                        if (controller.create) {
-                            controller.create(req, res);
-                        }
-                    });
-                    app.put('/' + block_folder + '/' + controller_name + '/:id', attachDB, function (req, res) {
-                        if (controller.update) {
-                            controller.update(req, res);
-                        }
-                    });
-                    app.delete('/' + block_folder + '/' + controller_name + '/:id', attachDB, function (req, res) {
-                        if (controller.destroy) {
-                            controller.destroy(req, res);
-                        }
-                    });
-                })(controller_name, controller);
-            }
-        }
-
-//lib kernel webservices
-        var controllers = fs.readdirSync(path.join(__dirname, 'lib', 'controllers'));
         for (var c in controllers) {
             var controller_name = controllers[c].replace('.js', '');
-            var controller = require('./lib/controllers' + '/' + controller_name);
+            var controller = require(path.join(process.cwd(), 'blocks', block_folder, 'backend', 'controllers', controller_name));
             (function (controller_name, controller) {
-                app.get('/' + controller_name, attachDB, function (req, res) {
+                app.get('/' + block_folder + '/' + controller_name, function (req, res) {
                     if (controller.index) {
                         controller.index(req, res);
                     }
                 });
-                app.all('/' + controller_name + '/action/:action', attachDB, function (req, res) {
+                app.all('/' + block_folder + '/' + controller_name + '/action/:action', function (req, res) {
                     if (controller[req.params.action]) {
                         controller[req.params.action](req, res);
                     }
                 });
-                app.get('/' + controller_name + '/:id', attachDB, function (req, res) {
+                app.get('/' + block_folder + '/' + controller_name + '/:id', function (req, res) {
                     if (controller.show) {
                         controller.show(req, res);
                     }
                 });
-                app.post('/' + controller_name, attachDB, function (req, res) {
+                app.post('/' + block_folder + '/' + controller_name, function (req, res) {
                     if (controller.create) {
                         controller.create(req, res);
                     }
                 });
-                app.put('/' + controller_name + '/:id', attachDB, function (req, res) {
+                app.put('/' + block_folder + '/' + controller_name + '/:id', function (req, res) {
                     if (controller.update) {
                         controller.update(req, res);
                     }
                 });
-                app.delete('/' + controller_name + '/:id', attachDB, function (req, res) {
+                app.delete('/' + block_folder + '/' + controller_name + '/:id', function (req, res) {
                     if (controller.destroy) {
                         controller.destroy(req, res);
                     }
                 });
             })(controller_name, controller);
         }
-// [end] Implementing RESTful Web Services with controllers in lib and backend folder.  ****/
+    }
 
-        app.all('/', function (req, res) {
-            res.render('index', {
-                site: config.site
-            });
-        });
-
-        var server = http.createServer(app);
-        server.listen(config.port, function () {
-            console.log(' __   __   __\n' +
-                '|__| |__| |__|\n' +
-                ' __   __   __\n' +
-                '|__| |__| |__|\n' +
-                ' __   __   __\n' +
-                '|__| |__| |__|\n\n' +
-                ' SMART BLOCKS\nRunning on ' + app.get('port'));
-        });
-
-        var io = require('socket.io').listen(server, { log: false });
-
-        io.sockets.on('connection', function (socket) {
-            socket.on('set id', function (session_id) {
-                socket.set('id', session_id);
-            });
-
-            socket.on('send_message', function (session_id, message) {
-                var clients = io.sockets.clients();
-                for (var k in clients) {
-                    var client = clients[k];
-                    client.get('id', function (err, id) {
-                        if (id == session_id) {
-                            client.emit("msg", message);
-                        }
-                    });
+//lib kernel webservices
+    var controllers = fs.readdirSync(path.join(__dirname, 'lib', 'controllers'));
+    for (var c in controllers) {
+        var controller_name = controllers[c].replace('.js', '');
+        var controller = require('./lib/controllers' + '/' + controller_name);
+        (function (controller_name, controller) {
+            app.get('/' + controller_name, function (req, res) {
+                if (controller.index) {
+                    controller.index(req, res);
                 }
             });
-
-            socket.on('broadcast_message', function (message) {
-                io.sockets.emit('msg', message);
+            app.all('/' + controller_name + '/action/:action', function (req, res) {
+                if (controller[req.params.action]) {
+                    controller[req.params.action](req, res);
+                }
             });
+            app.get('/' + controller_name + '/:id', function (req, res) {
+                if (controller.show) {
+                    controller.show(req, res);
+                }
+            });
+            app.post('/' + controller_name, function (req, res) {
+                if (controller.create) {
+                    controller.create(req, res);
+                }
+            });
+            app.put('/' + controller_name + '/:id', function (req, res) {
+                if (controller.update) {
+                    controller.update(req, res);
+                }
+            });
+            app.delete('/' + controller_name + '/:id', function (req, res) {
+                if (controller.destroy) {
+                    controller.destroy(req, res);
+                }
+            });
+        })(controller_name, controller);
+    }
+// [end] Implementing RESTful Web Services with controllers in lib and backend folder.  ****/
+
+    app.all('/', function (req, res) {
+        res.render('index', {
+            site: config.site
+        });
+    });
+
+    var server = http.createServer(app);
+    server.listen(config.port, function () {
+        console.log(' __   __   __\n' +
+            '|__| |__| |__|\n' +
+            ' __   __   __\n' +
+            '|__| |__| |__|\n' +
+            ' __   __   __\n' +
+            '|__| |__| |__|\n\n' +
+            ' SMART BLOCKS\nRunning on ' + app.get('port'));
+    });
+
+    var io = require('socket.io').listen(server, { log: false });
+
+    io.sockets.on('connection', function (socket) {
+        socket.on('set id', function (session_id) {
+            socket.set('id', session_id);
         });
 
+        socket.on('send_message', function (session_id, message) {
+            var clients = io.sockets.clients();
+            for (var k in clients) {
+                var client = clients[k];
+                client.get('id', function (err, id) {
+                    if (id == session_id) {
+                        client.emit("msg", message);
+                    }
+                });
+            }
+        });
 
+        socket.on('broadcast_message', function (message) {
+            io.sockets.emit('msg', message);
+        });
     });
+
+
 };
--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-var app_starter = require('./app');
-var fs = require('fs');
-var path = require('path');
-var smartblocks = require('./lib/smartblocks');
+/**
+ * This module is a public object that will give the developer access to several important objects, like
+ * models, or other stuff.
+ */
 
-var cwd = process.cwd();
+var smartblocks = {
+        models: require('lib/models/access')
+    };
 
-
-var program = require('commander');
-var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
-module.exports = function () {
-    program
-        .version(pkg.version)
-        .option('start', 'Start application')
-        .option('init [name]', 'Create a new SmartBlocks project in the folder [name]')
-        .option('create_block [name]', 'Create a new block structure in the folder blocks / [name]')
-        .parse(process.argv);
-
-    if (program.start) {
-        console.log('Web app starting, use Ctr-C to stop');
-        app_starter();
-    }
-
-    if (program.create_block) {
-        smartblocks.cli().createBlock(program.create_block);
-    }
-
-    if (program.init) {
-        if (program.init === true) {
-            program.init = '.';
-        }
-        smartblocks.cli().createProjectStructure(program.init);
-    }
-};
+module.exports = smartblocks;
