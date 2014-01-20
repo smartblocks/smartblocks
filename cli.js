83fecf4f9ee5c7cc130a90972a3702ceff618907
Antoine Jackson <a.j.william26@gmail.com>
2014-01-20 02:55:25 +0100

    + Added view generation in cli

--- cli.js
+++ cli.js
@@ -13,12 +13,13 @@ module.exports = function () {
     program
         .version(pkg.version)
         .option('start', 'Start application')
-        .option('init [name]', 'Create a new SmartBlocks project in the folder [name]')
-        .option('generate_block', 'Create a new block structure in the folder blocks')
+        .option('init [name]', 'Create a new SmartBlocks project in the folder [name].')
+        .option('generate_block', 'Create a new block structure in the folder blocks.')
         .option('generate_type', 'Generates a type in the block of your choice, with associated backend/frontend models, controllers and so on.')
-        .option('generate_app', 'Generates a new app in the front end of the block of your choice, with an entry view, template and style')
-        .option('generate_controller', 'Generates a new controller in the block of your choice')
-        .option('generate_model', 'Generates a new model in the block of your choice')
+        .option('generate_app', 'Generates a new app in the front end of the block of your choice, with an entry view, template and style.')
+        .option('generate_controller', 'Generates a new controller in the block of your choice.')
+        .option('generate_model', 'Generates a new model in the block of your choice.')
+        .option('generate_view', 'Generates view files (Backbone view, template and style file) in the block and app of your choice.')
         .parse(process.argv);
 
     if (program.start) {
@@ -53,8 +54,14 @@ module.exports = function () {
         generation.generateModel();
     }
 
+    else if (program.generate_view) {
+        generation.generateView();
+    }
+
     else {
         console.log('Usage : smartblocks' + ' command'.yellow);
-        console.log('with command: ' + 'init'.green + '|' + 'start'.green + '|' + 'generate_block'.green + '|' + 'generate_type'.green + '|' + 'generate_app'.green);
+        console.log('with command: ' + 'init'.green + '|' + 'start'.green + '|' + 'generate_block'.green +
+            '|' + 'generate_type'.green + '|' + 'generate_app'.green + '|' + 'generate_controller'.green +
+            '|' + 'generate_model'.green + '|' + 'generate_view'.green);
     }
 };
