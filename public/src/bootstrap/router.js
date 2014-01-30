b064ee6cdda6e619b0dc5b8d7bce13eae0d40be0
Antoine Jackson <a.j.william26@gmail.com>
2014-01-30 23:44:10 +0100

    Added way to specify root app from config (preferable)

--- public/src/bootstrap/router.js
+++ public/src/bootstrap/router.js
@@ -5,9 +5,11 @@ define([
     var initRouter = function (SmartBlocks) {
         SmartBlocks.basics.Router = Backbone.Router.extend({
             routes: {
+                '': 'entry',
+                '!': 'entry'
             },
             initialize: function () {
-                this.route(/^!(.*?)$/, "launch_app", this.launch_app);
+                this.route(/^!(..*?)$/, "launch_app", this.launch_app);
                 this.routesHit = 0;
                 Backbone.history.on('route', function () {
                     this.routesHit++;
@@ -27,6 +29,24 @@ define([
                         SmartBlocks.Url.params = params ? params.split('/') : [];
                         SmartBlocks.Url.appname = app.get('name');
                         SmartBlocks.Url.full = route;
+
+                        if (app && (!SmartBlocks.current_app || SmartBlocks.current_app.get("name") != app.get("name"))) {
+                            app = SmartBlocks.Data.apps.get(app.get('name'));
+                            SmartBlocks.Methods.setApp(app);
+                            break;
+                        }
+                    }
+                }
+            },
+            entry: function () {
+                var apps = SmartBlocks.Data.apps.models;
+
+                for (var k in apps) {
+                    var app = apps[k];
+                    if (app.get('name') == SmartBlocks.Config.entry.app && app.get('block_token') == SmartBlocks.Config.entry.block) {
+                        SmartBlocks.Url.params = SmartBlocks.Config.entry.url_params;
+                        SmartBlocks.Url.appname = app.get('name');
+                        SmartBlocks.Url.full = "";
 
                         if (app && (!SmartBlocks.current_app || SmartBlocks.current_app.get("name") != app.get("name"))) {
                             app = SmartBlocks.Data.apps.get(app.get('name'));
